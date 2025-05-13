1da10a3881b89870ea3127298db01e678c87dc17
Natalie8a <naty.8a@outlook.com>
2025-05-13 15:19:29 -0500

    another image successfully placed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Inicio</span>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-light">Nosotros</button>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-light">Servicios</button>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-light">Metodología</button>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-light">Proceso de inscripción</button>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-info">Contacto</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,13 @@ import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  return (
+    <div className="text-center mt-5">
+      <p>
+        <img src="src/assets/img/SVGlogo.svg" alt="Logo" width="600px" />
+      </p>
+    </div>
+  );
+};
