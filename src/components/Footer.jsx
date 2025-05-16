844869c96105dd0884ac5ef3feb85fffd81eb614
Natalie8a <naty.8a@outlook.com>
2025-05-16 01:44:21 -0500

    changed to N8A

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,7 @@ export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <WhatsApp />
     <p className="credits">
-      Sitio hecho por{" "}
-      <a href="https://github.com/Natalie8a/Natalie8a">Natalie Ochoa</a>
+      Sitio hecho por <a href="https://github.com/Natalie8a/Natalie8a">N8a</a>
     </p>
   </footer>
 );
